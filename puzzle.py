99a5de1db3f9eef665754407a5546526e7d6dad8
Farman Allard <fallard@oa-l5.msk.21-school.ru>
2021-10-09 17:52:47 +0300

    refactoring

--- puzzle.py
+++ puzzle.py
@@ -1,99 +1,147 @@
 from collections import deque
 
-row = 0
-col = 0
-
 class Board():
 	def __init__(self, new_board, new_parent=None, new_action=None):
-		self.board = new_board
+		self.board = self.copy_board(new_board) # Копирование игрового поля
+		self.height = len(self.board) # высота
+		self.width = len(self.board[0]) # ширина
 		self.parent = new_parent
-		self.action = new_action
+		# self.action = new_action
 		self.h = 0
-		self.g = 0 if not self.parent else self.parent.g + 1
-		self.f = 0
-		self.zero_x = 0 # ?
-		self.zero_y = 0 # ?
+		self.g = 0 if not new_parent else new_parent.g + 1
+		self.zero_x = -1 if not new_parent else new_parent.zero_x # координата пустой клетки по x
+		self.zero_y = -1 if not new_parent else new_parent.zero_y # координата пустой клетки по y
+		if not new_parent:
+			self.find_zero()
+		self.do_action(new_action)
+		self.calculate_h()
 
-		for i in range(row):
-			for j in range(col):
-				if (self.board[i][j] != i * row + j+ 1) and (self.board[i][j] != 0):
-					self.h += 1
-				if not self.board[i][j]: # ?
-					self.zero_x = i # ?
-					self.zero_y = j # ?
-		self.f = self.g + self.h
+	def get_board(self):
+		return self.board
+
+	def get_board_sizes(self) -> tuple:
+		return (self.width, self.height)
 
 	def get_h(self):
 		return self.h
 
-	def get_g(self):
-		return self.g
+	# def get_g(self):
+	# 	return self.g
 	
 	def get_f(self):
-		return self.f
+		return self.g + self.h
 
-	def get_zero(self) -> tuple:
-		return (self.zero_y, self.zero_x)
+	def get_zero_x(self):
+		return self.zero_x
 
-	def print_board(self):
-		for row in self.board:
-			print(*row)
+	def get_zero_y(self):
+		return self.zero_y
 
+	def get_zero_coords(self) -> tuple:
+		return (self.zero_x, self.zero_y)
+
+	# Возвращаем игровое поле как новый объект
+	def copy_board(self, copy_board):
+		new_copy = []
+		for row in copy_board:
+			new_copy.append(list(row))
+		return new_copy
+
+	def calculate_h(self):
+		for i in range(self.height):
+			for j in range(self.width):
+				if (self.board[i][j] != i * self.height + j + 1) and (self.board[i][j] != 0):
+					self.h += 1
+		if self.board[self.height - 1][self.width - 1] != 0:
+			self.h += 1
+
+	def find_zero(self,):
+		if self.zero_x < 0 or self.zero_y < 0:
+			for i in range(self.height):
+				for j in range(self.width):
+					if not self.board[i][j]:
+						self.zero_x = j
+						self.zero_y = i
+		
 	def do_action(self, action):
+		board = self.board
+		x, y = self.zero_x, self.zero_y
 		if action == 'right':
-			pass
+			board[y][x + 1], board[y][x] = board[y][x], board[y][x + 1]
+			self.zero_x, self.zero_y = self.zero_x + 1, self.zero_y
 		elif action == 'left':
-			pass
+			board[y][x - 1], board[y][x] = board[y][x], board[y][x - 1]
+			self.zero_x, self.zero_y = self.zero_x - 1, self.zero_y
 		elif action == 'up':
-			pass
+			board[y - 1][x], board[y][x] = board[y][x], board[y - 1][x]
+			self.zero_x, self.zero_y = self.zero_x, self.zero_y - 1
 		elif action == 'down':
-			pass
+			board[y + 1][x], board[y][x] = board[y][x], board[y + 1][x]
+			self.zero_x, self.zero_y = self.zero_x, self.zero_y + 1
 
-	def print_stats(self):
-		self.print_board()
-		print('\n\033[31m=================\033[0m')
-		print("h =", self.h)
-		print("g =", self.g)
-		print("h =", self.h)
-		print("f =", self.f)
-		print("zero_x =", self.zero_x)
-		print("zero_y =", self.zero_y)
-		print(f"row = {row}, col = {col}")
-		print('parent =', self.parent)
-		print('\033[31m=================\033[0m')
+
+	# def print_stats(self):
+	# 	self.print_board()
+	# 	print('\n\033[31m=================\033[0m')
+	# 	print("h =", self.h)
+	# 	print("g =", self.g)
+	# 	print("h =", self.h)
+	# 	print("f =", self.f)
+	# 	print("zero_x =", self.zero_x)
+	# 	print("zero_y =", self.zero_y)
+	# 	print(f"row = {row}, col = {col}")
+	# 	print('parent =', self.parent)
+	# 	print('\033[31m=================\033[0m')
 
 	def __eq__(self, eq_board) -> bool:
 		return self.board == eq_board
 
 	def __str__(self) -> str:
-		return '\n'.join(map(str, self.board)) + f'\th = {self.h}, f = {self.f}'
+		return '\n'.join(map(str, self.board)) + f'\tg = {self.g}, h = {self.h}, f = {self.get_f()}'
 
-if __name__ == "__main__":
+
+def solver(board):
 	queue = deque()
-	opened = []
 	closed = []
-	board = [
-			[1, 2, 7],
-			[3, 4, 6],
-			[0, 8, 5]
-			]
-	row = 3
-	col = 3
-	node = Board(board)
-	queue.append(node)
-	
+	opened = []
+	queue.append(Board(board))
 	while len(queue) > 0:
 		current_node = queue.pop()
 		# print(current_node)
 		if current_node.get_h() == 0:
 			break
-		# print(current_node.get_zero())
-		zero = current_node.get_zero()
-		if zero[1] > 0: # Сдвиг пустышки влево
-			pass
-		if zero[1] < row - 1: # Сдвиг пустышки вправо
-			pass
-		if zero[0] > 0: # Сдвиг пустышки вверх
-			pass
-		if zero[0] < col - 1: # Сдвиг пустышки вниз
-			pass
+		x, y = current_node.get_zero_coords()
+		w, h = current_node.get_board_sizes()
+
+		if x < w - 1: # Сдвиг вправо
+			node = Board(current_node.get_board(), current_node, 'right')
+			if node not in closed:
+				queue.append(node)
+		if x > 0: # Сдвиг влево
+			node = Board(current_node.get_board(), current_node, 'left')
+			if node not in closed:
+				queue.append(node)
+		if y > 0: # Сдвиг вверх
+			node = Board(current_node.get_board(), current_node, 'up')
+			if node not in closed:
+				queue.append(node)
+		if y < h - 1: # Сдвиг вниз
+			node = Board(current_node.get_board(), current_node, 'down')
+			if node not in closed:
+				queue.append(node)
+		
+		closed.append(current_node)
+		print(current_node)
+		input()
+	
+
+		
+if __name__ == "__main__":
+	
+	board = [
+			[1, 2, 7],
+			[3, 4, 6],
+			[0, 8, 5]
+			]
+	solver(board)
+	
